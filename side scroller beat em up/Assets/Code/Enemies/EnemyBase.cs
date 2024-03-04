8a9313881770e8d90af3442a59fbc443510c7355
JustFunPlay <dinandmeijer01@gmail.com>
2024-03-04 15:44:18 +0100

    Enemy activation and movement
    Updated EnemyBase class to add called activation and chasing the player

--- side scroller beat em up/Assets/Code/Enemies/EnemyBase.cs
+++ side scroller beat em up/Assets/Code/Enemies/EnemyBase.cs
@@ -5,19 +5,33 @@ using UnityEngine.AI;
 
 public class EnemyBase : MonoBehaviour
 {
-    Character closestCharacter;
-    [SerializeField]int health = 50;
-    [SerializeField]int damage;
-    [SerializeField]float attackRange;
-    [SerializeField] float attackDelay;
+    protected Character closestCharacter;
+    [SerializeField] protected int health = 50;
+    [SerializeField] protected int damage;
+    [SerializeField] protected float attackRange = 1.5f;
+    [SerializeField] protected float attackDelay;
 
     [SerializeField] int scoreOnHit;
     [SerializeField] int scoreOnKill;
 
-    NavMeshAgent agent;
-    bool isActive;
-    Animator animator;
-    
+    protected NavMeshAgent agent;
+    protected bool isActive;
+    protected Animator animator;
+
+    protected virtual void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        ActivateSelf();
+    }
+    protected virtual void Update()
+    {
+        if (agent != null && closestCharacter != null)
+        {
+            agent.SetDestination(closestCharacter.transform.position + (Vector3.right * (transform.position.x > closestCharacter.transform.position.x ? attackRange : -attackRange) ));
+        }
+    }
+
     public void TakeDamage(int damageToDo, Character attacker)
     {
         health -= damageToDo;
@@ -30,8 +44,16 @@ public class EnemyBase : MonoBehaviour
         }
     }
 
-    private void OnDeath()
+    protected virtual void OnDeath()
     {
         Destroy(gameObject);
+    }
+
+    public void ActivateSelf()
+    {
+        if (isActive) return;
+        gameObject.SetActive(true);
+        closestCharacter = FindObjectOfType<Character>();
+        isActive = true;
     }
 }
