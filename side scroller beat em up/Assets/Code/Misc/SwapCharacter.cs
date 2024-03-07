9c41bf60fb918fd3cd2766e7086619e35f4ada30
JustFunPlay <dinandmeijer01@gmail.com>
2024-03-07 10:39:04 +0100

    Second player properly select-able

--- side scroller beat em up/Assets/Code/Misc/SwapCharacter.cs
+++ side scroller beat em up/Assets/Code/Misc/SwapCharacter.cs
@@ -5,45 +5,12 @@ using UnityEngine;
 public class SwapCharacter : MonoBehaviour
 {
     [SerializeField] Player playerSelecting;
-    [SerializeField] Character[] characters;
-    private void Start()
+    [SerializeField] Character selectedCharacter;
+    private void OnEnable()
     {
         if (playerSelecting == Player.player1)
-            GameManager.Player1Character = characters[0];
+            GameManager.Player1Character = selectedCharacter;
         else
-            GameManager.Player2Character = characters[0];
-    }
-    public void SwapToNextCharacter()
-    {
-        if (playerSelecting == Player.player1)
-        {
-            int i = 0;
-            Character selectedCharacter = null;
-            while (selectedCharacter == null)
-            {
-                if (characters[i] == GameManager.Player1Character)
-                {
-                    selectedCharacter = characters[i];
-                }
-                i++;
-            }
-            if (i == characters.Length) i = 0;
-            GameManager.Player1Character = characters[i];
-        }
-        else
-        {
-            int i = 0;
-            Character selectedCharacter = null;
-            while (selectedCharacter == null)
-            {
-                if (characters[i] == GameManager.Player2Character)
-                {
-                    selectedCharacter = characters[i];
-                }
-                i++;
-            }
-            if (i == characters.Length) i = 0;
-            GameManager.Player2Character = characters[i];
-        }
+            GameManager.Player2Character = selectedCharacter;
     }
 }
